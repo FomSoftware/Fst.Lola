e7223709b3ecee5e48597b7457a7af04c665f17b
Davide Puolo <davide.puolo@alten.it>
2019-03-19 22:13:58 +0100

    aggiunte modifiche database

---
+++ 002_Machine.sql
@@ -0,0 +1,9 @@
+﻿ALTER TABLE [dbo].[Machine] DROP CONSTRAINT [FK_Machine_Plant];
+GO
+
+ALTER TABLE [dbo].[Machine] ALTER COLUMN [PlantId] INT NULL;
+GO
+
+ALTER TABLE [dbo].[Machine] WITH NOCHECK
+    ADD CONSTRAINT [FK_Machine_Plant] FOREIGN KEY ([PlantId]) REFERENCES [dbo].[Plant] ([Id]);
+GO
