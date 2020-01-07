663e0adee5696f8b072e3705e5d21d2a8812ceb9
mbelletti-fst <mbelletti@fomsoftware.com>
2020-01-07 16:15:34 +0100

    Nuovo layout

--- FomMonitoring/GlobApp/scripts/machine/image-manager.js
+++ FomMonitoring/GlobApp/scripts/machine/image-manager.js
@@ -1,6 +1,23 @@
 ﻿var ImageManager = function () {
 
     var vmImageMachine;
+    var vmButtonsMenu;
+
+    var mixinDetictingMobile = {
+        methods: {
+            isMobile: function () {
+                var check = false;
+                (function (a) {
+
+                    if (/Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(a))
+                        check = true;
+
+                    console.log(a);
+                })(navigator.userAgent || navigator.vendor || window.opera);
+                return check;
+            }
+        }
+    }
 
     var selectMachineGroup = function (element) {
         var group = $(element).data('group');
@@ -19,15 +36,27 @@
 
     var initMachineImage = function () {
         $("g[data-group]").click(function (e) {
+            e.preventDefault();
             selectMachineGroup(this);
         });
 
         $(".machine-group-selection button").click(function (e) {
+            e.preventDefault();
             selectMachineGroup(this);
         });
 
         $("button[data-panel]").click(function (e) {
+            e.preventDefault();
             selectPanel(this);
+        });
+
+        $("#button-back-machine").click(function (e) {
+            if (vmImageMachine.machineGroupSelected != null || vmImageMachine.machinePanelSelected != null) {
+                e.preventDefault();
+                $("g[data-highlighted] path").css("fill", "transparent");
+                vmImageMachine.machineGroupSelected = null;
+                vmImageMachine.machinePanelSelected = null;
+            }
         });
     };
 
@@ -38,8 +67,15 @@
             data: {
                 machineGroupSelected: null,
                 machinePanelSelected: null
-            }
+            },
+            mixins: [mixinDetictingMobile]
         });
+
+        vmButtonsMenu = new Vue({
+            el: "#buttons-bar",
+            mixins: [mixinDetictingMobile]
+        });
+        
         initMachineImage();
     };
     
