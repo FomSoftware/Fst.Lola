2c43040ddaca3b8589c0c50a05cc5f9949e71bf0
mbelletti-fst <mbelletti@fomsoftware.com>
2020-01-20 10:48:07 +0100

    Modifiche a partial di entity framework

--- FomMonitoringCore/DAL/Partials/MessageMachine.cs
+++ FomMonitoringCore/DAL/Partials/MessageMachine.cs
@@ -7,28 +7,6 @@ using Newtonsoft.Json;
 
 namespace FomMonitoringCore.DAL
 {
-    public partial class HistoryMessage
-    {
-        public string GetDescription(int idLanguage)
-        {
-            var result = MessagesIndex.MessageTranslation.FirstOrDefault(t => t.MessageLanguageId == idLanguage)?.Translation;
-
-
-            if (result == null)
-                return string.Empty;
-
-
-            if (string.IsNullOrEmpty(Params))
-                return result;
-
-            var parDict =
-                JsonConvert.DeserializeObject<Dictionary<string, string>>(Params);
-
-
-            return parDict.Keys.Aggregate(result, (current, key) => current.Replace(key, parDict[key]));
-        }
-    }
-
     public partial class MessageMachine
     {
         public DateTime? GetInitialSpanDate(long PeriodicSpan)
