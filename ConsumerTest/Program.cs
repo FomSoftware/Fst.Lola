10fc5c83ad0078ee3af142d4be41a400cc55e343
mbelletti-fst <mbelletti@fomsoftware.com>
2020-02-27 08:48:38 +0100

    Modifica ioc

--- ConsumerTest/Program.cs
+++ ConsumerTest/Program.cs
@@ -15,55 +15,49 @@ namespace ConsumerTest
 {
     class Program
     {
+        
+
+        private static ILifetimeScope _scopeVariable;
+        private static ILifetimeScope _scopeInfo;
+        private static ILifetimeScope _scopeMessage;
+        private static ILifetimeScope _scopeHistoryJob;
+        private static ILifetimeScope _scopeTool;
+        private static ILifetimeScope _scopeState;
+        
+
         static void Main(string[] args)
         {
             var builder = new ContainerBuilder();
 
             FomMonitoringCore.Ioc.IocContainerBuilder.BuildCore(builder, false);
-            builder.RegisterType<QueueConnection>().As<IQueueConnection>().SingleInstance();
+            FomMonitoringCoreQueue.Ioc.IocContainerBuilder.BuildCore(builder, false);
 
-
-            builder.RegisterType<VariableListProducer>().As<IProducer<VariablesList>>().SingleInstance();
-            builder.RegisterType<InfoProducer>().As<IProducer<Info>>().SingleInstance();
-            builder.RegisterType<HistoryJobPieceBarProducer>().As<IProducer<HistoryJobPieceBar>>().SingleInstance();
-            builder.RegisterType<MessageProducer>().As<IProducer<Message>>().SingleInstance();
-            builder.RegisterType<StateProducer>().As<IProducer<State>>().SingleInstance();
-            builder.RegisterType<ToolProducer>().As<IProducer<Tool>>().SingleInstance();
-
-            builder.RegisterType<VariableListConsumer>().As<IConsumer<VariablesList>>().SingleInstance();
-            builder.RegisterType<InfoConsumer>().As<IConsumer<Info>>().SingleInstance();
-            builder.RegisterType<HistoryJobPieceBarConsumer>().As<IConsumer<HistoryJobPieceBar>>().SingleInstance();
-            builder.RegisterType<StateConsumer>().As<IConsumer<State>>().SingleInstance();
-            builder.RegisterType<MessageConsumer>().As<IConsumer<Message>>().SingleInstance();
-            builder.RegisterType<ToolConsumer>().As<IConsumer<Tool>>().SingleInstance();
-
-            builder.RegisterType<VariableListProcessor>().As<IProcessor<VariablesList>>().SingleInstance();
-            builder.RegisterType<InfoProcessor>().As<IProcessor<Info>>().SingleInstance();
-            builder.RegisterType<HistoryJobPieceBarProcessor>().As<IProcessor<HistoryJobPieceBar>>().SingleInstance();
-            builder.RegisterType<StateProcessor>().As<IProcessor<State>>().SingleInstance();
-            builder.RegisterType<MessageProcessor>().As<IProcessor<Message>>().SingleInstance();
-            builder.RegisterType<ToolProcessor>().As<IProcessor<Tool>>().SingleInstance();
-            builder.RegisterType<QueueForwarder>().As<IQueueForwarder>().SingleInstance();
             var container = builder.Build();
 
+            _scopeVariable = container.BeginLifetimeScope();
+            _scopeInfo = container.BeginLifetimeScope();
+            _scopeMessage = container.BeginLifetimeScope();
+            _scopeHistoryJob = container.BeginLifetimeScope();
+            _scopeTool = container.BeginLifetimeScope();
+            _scopeState = container.BeginLifetimeScope();
 
-            var consumerVariable = container.Resolve<IConsumer<VariablesList>>();
+            var consumerVariable = _scopeVariable.Resolve<IConsumer<VariablesList>>();
             consumerVariable.Init();
 
-            var consumerInfo = container.Resolve<IConsumer<Info>>();
+            var consumerInfo = _scopeInfo.Resolve<IConsumer<Info>>();
             consumerInfo.Init();
 
-            var consumerState = container.Resolve<IConsumer<State>>();
+            var consumerState = _scopeState.Resolve<IConsumer<State>>();
             consumerState.Init();
 
-            var consumerHistoryJob = container.Resolve<IConsumer<HistoryJobPieceBar>>();
+            var consumerHistoryJob = _scopeHistoryJob.Resolve<IConsumer<HistoryJobPieceBar>>();
             consumerHistoryJob.Init();
 
-            var consumerMessage = container.Resolve<IConsumer<Message>>();
+            var consumerMessage = _scopeMessage.Resolve<IConsumer<Message>>();
             consumerMessage.Init();
 
 
-            var consumerTool = container.Resolve<IConsumer<Tool>>();
+            var consumerTool = _scopeTool.Resolve<IConsumer<Tool>>();
             consumerTool.Init();
         }
     }
