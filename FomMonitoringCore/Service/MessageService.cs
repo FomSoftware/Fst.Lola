f9aaf55fce09febbbb2ede96c002836c30bfcb34
mbelletti-fst <mbelletti@fomsoftware.com>
2020-03-18 14:42:09 +0100

    IS-944

--- FomMonitoringCore/Service/MessageService.cs
+++ FomMonitoringCore/Service/MessageService.cs
@@ -414,7 +414,8 @@ namespace FomMonitoringCore.Service
             var notificationsUser = GetMaintenanceMessages(machine, period);
             var notificationsRead = _context.Set<MessageMachineNotification>().Where(n => n.UserId == userId)
                 .Select(nu => nu.IdMessageMachine).ToList();
-            notificationsUser = notificationsUser.Where(o => notificationsRead.All(i => i != o.Id)).ToList();
+            notificationsUser = notificationsUser.Where(o => notificationsRead.All(i => i != o.Id)).OrderBy(n => n.MachineId).ThenByDescending(n => n.Day).ToList();
+
             return notificationsUser;
         }
 
