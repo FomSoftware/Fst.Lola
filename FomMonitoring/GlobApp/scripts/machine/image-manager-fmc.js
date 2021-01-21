1b99d5b7154724bbc5f8735449539aab745991cc
fabianabugli <fbugli@fomsoftware.com>
2021-01-21 16:37:23 +0100

    aggiunte immagini fmc650

--- FomMonitoring/GlobApp/scripts/machine/image-manager-fmc.js
+++ FomMonitoring/GlobApp/scripts/machine/image-manager-fmc.js
@@ -8,7 +8,8 @@ ImageManFmc.prototype.checkModello = function (machineGroupSelected) {
 
     if (machineGroupSelected == 'FMC3-4_axes' ||
         machineGroupSelected == 'FMC2_axes' ||
-        machineGroupSelected == 'FMC1_axes') {
+        machineGroupSelected == 'FMC1_axes' ||
+        machineGroupSelected == 'FMC650_axes') {
             OtherData.show();
         } else {
             OtherData.hide();
@@ -16,7 +17,8 @@ ImageManFmc.prototype.checkModello = function (machineGroupSelected) {
 
     if (machineGroupSelected == 'FMC3-4_spindles' ||
         machineGroupSelected == 'FMC2_spindles' ||
-        machineGroupSelected == 'FMC1_spindles') {
+        machineGroupSelected == 'FMC1_spindles' ||
+        machineGroupSelected == 'FMC650_spindle') {
             ElectroSpindle.show();
         } else {
             ElectroSpindle.hide();
@@ -24,11 +26,17 @@ ImageManFmc.prototype.checkModello = function (machineGroupSelected) {
 
     if (machineGroupSelected == 'FMC3-4_tools' ||
         machineGroupSelected == 'FMC2_tools' ||
-        machineGroupSelected == 'FMC1_tools') {
+        machineGroupSelected == 'FMC1_tools' ||
+        machineGroupSelected == 'FMC650_tools') {
             ToolsFmcLmx.show();
         } else {
             ToolsFmcLmx.hide();
-        }
+    }
+    if (machineGroupSelected == 'FMC650_Motor_Blade') {
+        //ToolsFmcLmx.show();
+    } else {
+        //ToolsFmcLmx.hide();
+    }
 
 };
 
